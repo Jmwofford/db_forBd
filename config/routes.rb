fc6010625434b43b2ca8deb35c6a96a5a82c5dac
Jordan_M_Wofford <jmwofford@gmail.com>
2019-06-18 14:46:15 -0500

    login and registration complete

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/' => 'users#index'
-  # get '/index'=> 'sessions#index'
-  # post '/sessions' => 'sessions#create'
-  # post '/users' => 'users#create'
-  # get '/dashboard' => 'profiles#index'
+  get '/index'=> 'sessions#index'
+  post '/sessions' => 'sessions#create'
+  post '/users' => 'users#create'
+  get '/dashboard' => 'profiles#index'
 
 
 end
