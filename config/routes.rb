03bef5ff81ac5afb5e8ca27d5c7c3708c07041a3
Jordan_M_Wofford <jmwofford@gmail.com>
2019-06-18 14:10:07 -0500

    basic routing initialized:

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/' => 'users#index'
+  # get '/index'=> 'sessions#index'
+  # post '/sessions' => 'sessions#create'
+  # post '/users' => 'users#create'
+  # get '/dashboard' => 'profiles#index'
+
+
 end
